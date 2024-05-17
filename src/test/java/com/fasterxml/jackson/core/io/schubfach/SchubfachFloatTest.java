ab1c5bec1bdc5b3427a08c2ce15bf2d10c974916
timo <1398557+timo-a@users.noreply.github.com>
2024-05-17 21:11:31 +0200

    delete tests for speed

--- src/test/java/com/fasterxml/jackson/core/io/schubfach/SchubfachFloatTest.java
+++
@@ -1,8 +0,0 @@
-package com.fasterxml.jackson.core.io.schubfach;
-
-public class SchubfachFloatTest extends FloatToStringTest {
-  @Override
-  String f(float f) {
-    return FloatToDecimal.toString(f);
-  }
-}
